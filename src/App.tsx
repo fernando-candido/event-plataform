8da3d7b9dbd83e114437ee18ff587134b7574b73
Fernando <oficialfcsj@gmail.com>
2022-06-22 18:58:40 -0400

    iniciando criação de components

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,17 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql` 
-  query{
-    lessons{
-    id
-    title
-    teacher{
-      name
-    }
-    
-
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Header } from "./components/Header"
+import { Lesson } from "./components/Lesson"
+import { Sidebar } from "./components/Sidebar"
+import { Video } from "./components/Video"
 
 function App() {
-  // useEffect(() => {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY,
-  //   }).then(response => {
-  //     console.log(response.data)
-  //   })
-  // }, [])
-
-  const {data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
-  console.log(data);
   return (
-    // <h1 className="text-5xl font-bold text-blue-500" > Hello World</h1>
-    // <h1 className="title" > Hello World</h1>
-    <ul>
-      {data?.lessons.map(lesson =>{
-        return <li key={lesson.id}> {lesson.title}</li>
-      })}
-    </ul>
+    <div>
+      <Header />
+      <Lesson />
+      <Sidebar />
+      <Video />
+
+    </div>
   )
 }
 
